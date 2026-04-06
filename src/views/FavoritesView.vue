332a908893a776b474e8d1e58f588221947f69c6
NyaNyagulugulu <support@cnmsb.xin>
2026-04-07 01:57:16 +0800

    标准化替换

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -7,7 +7,7 @@
       </div>
       <div class="playlist-info">
         <div class="playlist-type">{{ t('key.playlists') }}</div>
-        <h1 class="playlist-title">{{ t('favorites.myFavorites') }}</h1>
+        <h1 class="playlist-title">{{ t('key.myFavorites') }}</h1>
         <div class="playlist-meta">
           <span class="music-count">{{ t('key.songs') }} {{ musicList.length }}</span>
         </div>
@@ -29,7 +29,7 @@
           <path fill="currentColor" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
         </svg>
       </div>
-      <p>{{ t('favorites.noFavorites') }}</p>
+      <p>{{ t('key.noFavorites') }}</p>
       <button class="btn-explore" @click="$router.push('/')">{{ t('key.discoverMusic') }}</button>
     </div>
 
